32fdc90e65f0a28a4884faa3fc21ca8f558d2183
Abhay Singh <1995.abhaysingh@gmail.com>
2023-09-19 00:28:48 +0530

    added logos

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,36 +1,46 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import "./Login.scss";
+import Banner  from "./../../Assest/Image/banner.jpg";
+import Logo from './../../Assest/Image/Logo.png'
 export const Login = () => {
   return (
     <div className="main">
       <Container className="main-container">
         <Row>
           <Col className="login-page-form">
-            <div>
-              <div className="credential-box">
-                <div className="input-with-icon">
-                  <span className="input-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                    </svg>
-                  </span>
-                  <input type="text" placeholder="Your login or e-mail" />
-                </div>
-                <div className="input-with-icon">
-                  <span className="input-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
-                      <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z" />
-                      <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                    </svg>
-                  </span>
-                  <input placeholder="Password"></input>
+            {/* <div> */}
+            <div className="credential-box">
+              <div class="input-icons">
+                <i class="bi bi-person"></i>
+                <input className="input-field" type="text" placeholder="Your login or e-mail" />
+              </div>
+              <div class="input-icons">
+                <i class="bi bi-key"></i>
+                <input className="input-field" type="text" placeholder="Password" />
+              </div>
+              <div>
+                <div className="submit-row" style={{}}>
+                  <Row>
+                    <Col className="register-forgot-parent" md={8} style={{}}>
+                      <span className="register-text">Register</span>
+                      <span className="forgot-text">Forgot Password</span>
+                    </Col>
+                    <Col className="submit-text" md={4}>
+                      <button className="btn btn-primary">Submit</button>
+                    </Col>
+                  </Row>
                 </div>
               </div>
-              Submit
+              {/* </div> */}
             </div>
           </Col>
-          <Col className="login-page-banner">Right</Col>
+          <Col className="login-page-banner">
+            <Image src={Banner} style={{objectFit:"cover", borderRadius:"0px 10px 10px 0px"}} width="100%" height="100%"/>
+            <div className="logo">
+              <Image src={Logo}></Image>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>
